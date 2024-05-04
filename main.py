193caa3aa36dade1aaf153728ccba12e5c2305dd
MAI GABBAR <141297334+the-gabbar@users.noreply.github.com>
2024-05-04 12:36:21 +0530

    Update main.py
    Fixed ⚡️ import error

--- main.py
+++ main.py
@@ -791,5 +791,5 @@ async def account_login(bot: Client, m: Message):
     except Exception as e:
         await m.reply_text(str(e))
     await m.reply_text("Done")
-    
-bot.run()    
+print("Successfully deployed bot")    
+bot.run()
